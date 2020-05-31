59ee081d4ed68aad8c123d795ab18c3ccb45de03
IgorBrykin <igorbrikin@gmail.com>
2020-05-31 14:08:38 +0300

    Home Work

--- src/five/homework/ArrayCopy.java
+++ src/five/homework/ArrayCopy.java
@@ -1,5 +1,14 @@
 package five.homework;
 
 public class ArrayCopy {
+    static final int size = 10000000;
+    static final int h = size / 2;
 
+    public void spot() {
+
+    }
+
+    public void fillIn() {
+
+    }
 }
